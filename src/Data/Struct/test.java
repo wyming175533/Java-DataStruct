70c929cdc65d22a04054265e8fcbea2aa1293156
wangyiming <1755337368@qq.com>
2022-02-11 18:58:16 +0800

    test two

--- src/Data/Struct/test.java
+++ src/Data/Struct/test.java
@@ -2,6 +2,9 @@ package Data.Struct;
 
 public class test {
     public static void main(String[] args) {
+
         System.out.println("branch输出。。。");
+        System.out.println("test two");
     }
+
 }
